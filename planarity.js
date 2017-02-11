d968adbc555a6c4d616846e40cb1a8ce8ac68864
Abhijeet Talaulikar <abhijeetstalaulikar@gmail.com>
2017-02-11 21:05:08 +0530

    Update planarity.js

--- planarity.js
+++ planarity.js
@@ -3,8 +3,8 @@ Number.prototype.between = function (min, max) {
 };
 
 n = Math.floor((Math.random() * 100) % 2);
-if(n) n = 9;
-else n = 8;
+if(n) n = 10;
+else n = 9;
 m = 2 * n - 4;
 vertices = new Array(n);
 for(i = 0;i < n;i++) vertices[i] = [0, 0];
